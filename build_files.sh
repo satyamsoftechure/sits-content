d17ee55535147bb8a53c489f052d9d2f0c286da1
satyamsoftechure <satyam.softechure@gmail.com>
2025-04-21 18:01:53 +0530

    versel

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,5 @@
- echo "BUILD START"
- python3.9 -m pip install -r requirements.txt
- python3.9 manage.py collectstatic --noinput --clear
- echo "BUILD END"
+echo "BUILD START"
+python3.9 -m pip install -r requirements.txt
+python3.9 manage.py collectstatic --noinput --clear
+mkdir -p staticfiles_build
+echo "BUILD END"
